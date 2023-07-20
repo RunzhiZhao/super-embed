02cc56d7bcd7d07ad6005271974fe89adb5bd1c5
runzhi.zhao <zhaorz.gd@gmail.com>
2023-07-20 15:47:47 +0800

    Initial commit

---
+++ src/Component.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { getContext } from "svelte"
+
+  const { styleable } = getContext("sdk")
+  const component = getContext("component")
+
+  export let text
+</script>
+
+<div class="embed" use:styleable={$component.styles}>
+  {@html text}
+</div>
+
+<style>
+  .embed {
+    position: relative;
+  }
+  .embed :global(> *) {
+    width: 100%;
+    height: 100%;
+  }
+</style>
